6b11f8e7b19c970c6ecc1c9279cd152d9cd312e4
adrianVelasco <64821523+Luadveand@users.noreply.github.com>
2022-12-05 00:32:01 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+// class public Objeto with id, nombre, descripcion, precio
+
+Console.WriteLine("Hello, World!");
+// JUGADOR
+Console.WriteLine("Ingrese su nombre");
+string? jugador = Console.ReadLine();
+Console.WriteLine("Hola " + jugador);
+
+
+
+
+string json = System.IO.File.ReadAllText("data.json");
+//Console.WriteLine(json);
+// agregar json a un arreglo
+string[]? data = System.Text.Json.JsonSerializer.Deserialize<string[]>(json);
+// mostrar data
+Console.WriteLine(data[0]);
+
